4278050621b6657efd5694c87f3c4d919f53e824
akradin <alex.kradin@gmail.com>
2016-11-24 12:47:23 -0500

    Add CRUD functionality for chores
    Create curl requests

--- rails-api-template-master/config/routes.rb
+++ rails-api-template-master/config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  # chores routes
+
+  get '/chores', to: 'chores#index'
+  get '/chores/:id', to: 'chores#show'
+  post '/chores', to: 'chores#create'
+  patch 'chores/:id', to: 'chores#update'
+  delete 'chores/:id', to: 'chores#destroy'
 end
--- rails-api-template-master/db/schema.rb
+++ rails-api-template-master/db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327214720) do
+ActiveRecord::Schema.define(version: 20161124173056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chores", force: :cascade do |t|
+    t.text     "name"
+    t.date     "due_date"
+    t.integer  "difficulty"
+    t.text     "assigned_to"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
